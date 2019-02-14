c436906f792512cb6c668f995f60107d32c9ad70
Curtis Adkins <curtis@wayne-manor.gotham>
2019-02-14 13:44:03 -0500

    fixed echo error

--- current_directory.sh
+++ current_directory.sh
@@ -4,8 +4,8 @@ CURRENT_DIR=${PWD}
 echo "The current directory is : "
 echo $CURRENT_DIR
 #adding a comment here
-echo "Current directory complete"
-#we are testing vscode now
+    echo "Current directory complete"
+    #we are testing vscode now
 echo "VS Code is pretty cool!"
 # integrate with git
-ecoh "Git is cool!"
+echo "Git is cool!"
